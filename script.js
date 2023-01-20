16de3c190fa49395929115b0a1f58b288f5a88d5
Greyseth <122962698+gilland-seth@users.noreply.github.com>
2023-01-20 21:54:56 +0700

    Unfinished commit
    mmmmmmmmmmmmmmmmmmmmmicrowave

---
+++ script.js
@@ -0,0 +1,90 @@
+let people = [
+  // {
+  //   name: "walter",
+  //   points: 0,
+  //   id: "a_num",
+  // },
+  // {
+  //   name: "mike",
+  //   points: 0,
+  //   id: "b_num",
+  // },
+  // {
+  //   name: "saul",
+  //   points: 0,
+  //   id: "c_num",
+  // },
+  // {
+  //   name: "gus",
+  //   points: 0,
+  //   id: "d_num",
+  // },
+  // {
+  //   name: "jr",
+  //   points: 0,
+  //   id: "e_num",
+  // },
+];
+
+changeDisplay();
+
+function shuffle(o) {
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
+}
+
+function changeDisplay() {
+  people.forEach(function (el) {
+    const name = document.getElementById(el.id);
+    name.innerHTML = el.name;
+
+    const num = document.getElementById(`${el.id}`);
+    num.innerHTML = el.points;
+  });
+}
+
+function randomize() {
+  const seed = people.map((el) => people.indexOf(el));
+
+  const selection =
+    people.length === 1
+      ? 0
+      : /*Math.floor(Math.random() * people.length)*/ shuffle(seed);
+
+  // document.getElementById(
+  //   `${people[selection].id}_num`
+  // ).style.animationIterationCount = 999;
+
+  people[selection].points++;
+
+  // document.getElementById(`${people[selection].id}_num`).animation =
+  //   "span-scale 1s 1 ease-out forward";
+
+  // setTimeout(function () {
+  //   document.getElementById(
+  //     `${people[selection].id}_num`
+  //   ).style.animationIterationCount = 0;
+  // });
+
+  changeDisplay();
+}
+
+function addMember() {
+  const name = document.getElementById("add-input").value;
+  people.push({ name: name, points: 0, id: `${name}_num` });
+
+  const toAdd = `
+  <li>
+    <p id="${name}_id">${name} Points:</p>
+    <span id="${name}_num">num</span>
+  </li>
+  `;
+
+  document.getElementById("names-list").insertAdjacentHTML("afterbegin", toAdd);
+
+  changeDisplay();
+}
